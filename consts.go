b3b3becbadad182c85175b2e335dfc1324c373c9
Linh Tran Tuan <linhduonggnu@gmail.com>
2018-11-28 18:14:15 +0900

    Update consts.go

--- consts.go
+++ consts.go
@@ -5,12 +5,9 @@ import (
 )
 
 var (
-	// ErrLatestNotSet latest not set
 	ErrLatestNotSet = fmt.Errorf("latest is not set yet")
 
-	// ErrQueryShouldSet indicates nil query
 	ErrQueryShouldSet = fmt.Errorf("query should not be nil")
 
-	// ErrWatcherIsClosed indicates watcher is closed
 	ErrWatcherIsClosed = fmt.Errorf("watcher is closed")
 )
