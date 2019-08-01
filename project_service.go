77e989f3fa6dbe43599e053d06043544affd4a5d
Ikhun Um <ikhun.um@linecorp.com>
2019-08-01 16:37:46 +0900

    Address comments by @linxGnu

--- project_service.go
+++ project_service.go
@@ -16,6 +16,7 @@ package centraldogma
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 )
 
@@ -52,7 +53,7 @@ func (p *projectService) create(ctx context.Context, name string) (*Project, int
 }
 
 func (p *projectService) remove(ctx context.Context, name string) (int, error) {
-	u := defaultPathPrefix + "projects/" + name
+	u := fmt.Sprintf("%sprojects/%s", defaultPathPrefix, name)
 
 	req, err := p.client.newRequest(http.MethodDelete, u, nil)
 	if err != nil {
@@ -67,7 +68,7 @@ func (p *projectService) remove(ctx context.Context, name string) (int, error) {
 }
 
 func (p *projectService) purge(ctx context.Context, name string) (int, error) {
-	u := defaultPathPrefix + "projects/" + name + "/removed"
+	u := fmt.Sprintf("%sprojects/%s/removed", defaultPathPrefix, name)
 
 	req, err := p.client.newRequest(http.MethodDelete, u, nil)
 	if err != nil {
@@ -82,7 +83,7 @@ func (p *projectService) purge(ctx context.Context, name string) (int, error) {
 }
 
 func (p *projectService) unremove(ctx context.Context, name string) (*Project, int, error) {
-	u := defaultPathPrefix + "projects/" + name
+	u := fmt.Sprintf("%sprojects/%s", defaultPathPrefix, name)
 
 	req, err := p.client.newRequest(http.MethodPatch, u, `[{"op":"replace", "path":"/status", "value":"active"}]`)
 	if err != nil {
